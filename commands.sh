de9721a4b3f3844366536f03e46013c83e1726a2
Maxim Yefremov <maxYefr@gmail.com>
2019-06-22 09:16:29 +0300

    added curl test

--- commands.sh
+++ commands.sh
@@ -20,4 +20,16 @@ runc(){
 	docker-compose up
 }
 
+test(){
+	curl -X POST   http://localhost:3000/tickets  -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' -d '{
+	"creator" : "Joel",
+	"title" : "Test ticket",
+	"description" : "A test ticket",
+	"points": 5
+	}'
+
+	curl http://localhost:3000/tickets
+	echo
+}
+
 $@
